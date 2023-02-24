9588660ea0993add23a5d8349f410df726d57b13
AZdevloper <ez.zahed.abdelaziz@gmail.com>
2023-02-24 15:42:28 +0100

    added comment rols

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -12,9 +12,8 @@ class CommentController extends Controller
 
      public function __construct()
     {
-        $this->middleware('auth');
-        $this->middleware('IsAdmin')->except(['index','store','update','destroy','show']);
-        $this->middleware('IsAuthor');
+        $this->middleware('auth:sanctum');
+        $this->middleware('IsAdmin')->only(['index','update','destroy']);
     }
     /**
      * Display a listing of the resource.
